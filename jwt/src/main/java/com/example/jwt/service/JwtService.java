453bcf0ddc49db63fa1a69dee329a47af8f99dee
이동현 <enthchal@gmail.com>
2024-05-14 15:36:38 +0900

    JWT Token 인증

---
+++ jwt/src/main/java/com/example/jwt/service/JwtService.java
@@ -0,0 +1,70 @@
+package com.example.jwt.service;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class JwtService {
+
+    private static String secretKey = "Java17SpringBootJWTTokenIssueMethod";
+
+    public String create(
+
+            Map<String, Object> claims,
+            LocalDateTime expireAt
+
+    ) {
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        var _expiredAt = Date.from(expireAt.atZone(ZoneId.systemDefault()).toInstant());
+
+        return Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setClaims(claims)
+                .setExpiration(_expiredAt)
+                .compact();
+    }
+
+    public void validation(String token) {
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        var parser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+
+        try {
+
+            var result = parser.parseClaimsJws(token);
+
+            result.getBody().entrySet().forEach(value -> {
+                log.info("key : {}, value : {}", value.getKey(), value.getValue());
+            });
+
+        }catch (Exception e) {
+
+            if(e instanceof SignatureException) {
+                throw new RuntimeException("JWT Token Not Valid Exception");
+            }
+            else if(e instanceof ExpiredJwtException){
+                throw new RuntimeException("JWT Token Expired Exception");
+            }
+            else {
+                throw new RuntimeException("JWT Token VAlidation Exception");
+            }
+
+        }
+    }
+}
---
+++ jwt/src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -0,0 +1,46 @@
+package com.example.jwt;
+
+import com.example.jwt.service.JwtService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.SimpleTimeZone;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void tokenCreate() {
+        var claims = new HashMap<String, Object>();
+        claims.put("user_id", 923);
+
+        var expiredAt = LocalDateTime.now().plusSeconds(30);
+
+        var jwtToken = jwtService.create(claims, expiredAt);
+
+        System.out.println(jwtToken);
+    }
+
+    @Test
+    void tokenValidation() {
+
+        var token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5MjMsImV4cCI6MTcxNTY2ODQ1M30.WcEqxPrbelbSuuL1tAtPNFSZm-PW52C2tUBj1x8LBI8";
+
+        jwtService.validation(token);
+
+
+
+    }
+
+}
