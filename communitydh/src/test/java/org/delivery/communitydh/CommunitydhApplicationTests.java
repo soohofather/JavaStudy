1a44104567be7f2cdadc6b9418fa0f55c253c8db
이동현 <enthchal@gmail.com>
2024-07-01 09:33:44 +0900

    communitydh

---
+++ communitydh/src/test/java/org/delivery/communitydh/CommunitydhApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.communitydh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitydhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
